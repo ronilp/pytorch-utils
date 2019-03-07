7cd1b0dbb45c08a091cb4f9f863a5e1854467681
Ronil <ronilp3@gmail.com>
2019-03-07 02:35:08 -0500

    load dataset from csv

--- dataset_utils.py
+++ dataset_utils.py
@@ -5,8 +5,9 @@ import multiprocessing
 import config
 
 
-def load_datasets(DatasetClass, transforms=None):
-    datasets = {x: DatasetClass(os.path.join(config.DATA_DIR, x), x, transforms) for x in ['train', 'val']}
+def load_datasets(DatasetClass, transforms=None, datasets=None):
+    if datasets is not None:
+        datasets = {x: DatasetClass(os.path.join(config.DATA_DIR, x), x, transforms) for x in ['train', 'val']}
     dataset_loaders = {
         x: torch.utils.data.DataLoader(datasets[x], batch_size=config.BATCH_SIZE, shuffle=True,
                                        num_workers=multiprocessing.cpu_count()) for x in ['train', 'val']}
@@ -14,10 +15,20 @@ def load_datasets(DatasetClass, transforms=None):
     return dataset_loaders, dataset_sizes
 
 
-def load_testset(DatasetClass, transforms=None):
-    datasets = {x: DatasetClass(os.path.join(config.DATA_DIR, x), x, transforms) for x in ['test']}
+def load_testset(DatasetClass, transforms=None, datasets=None):
+    if datasets is not None:
+        datasets = {x: DatasetClass(os.path.join(config.DATA_DIR, x), x, transforms) for x in ['test']}
     dataset_loaders = {
         x: torch.utils.data.DataLoader(datasets[x], batch_size=config.BATCH_SIZE * 4, shuffle=False,
                                        num_workers=multiprocessing.cpu_count()) for x in ['test']}
     dataset_sizes = {x: len(datasets[x]) for x in ['test']}
     return dataset_loaders, dataset_sizes
+
+def load_datasets_from_csv(DatasetClass, transforms=None):
+    datasets = {x: DatasetClass(config.DATA_DIR, x, transforms) for x in ['train', 'val']}
+    return load_datasets(DatasetClass, transforms, datasets)
+
+
+def load_testset_from_csv(DatasetClass, transforms=None):
+    datasets = {x: DatasetClass(config.DATA_DIR, x, transforms) for x in ['test']}
+    return load_datasets_from_csv(DatasetClass, transforms, datasets)
