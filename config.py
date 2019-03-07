7d2400b8676e0582c49cf5395dde9e0e11158609
Ronil <rpancho@ncsu.edu>
2019-03-07 02:54:29 -0500

    added basic configs

--- config.py
+++ config.py
@@ -3,5 +3,24 @@
 # Date: 3/7/19
 # Time: 2:46 AM
 
-DATA_DIR = ""
-BATCH_SIZE = 64
+import torch
+
+### Learning Parameters
+BASE_LR = 1e-4
+TRAIN_EPOCHS = 50
+EARLY_STOPPING_ENABLED = False
+EARLY_STOPPING_PATIENCE = 10
+
+### Dataset Config
+DATA_DIR = "data"
+ALLOWED_CLASSES = []
+NUM_CLASSES = len(ALLOWED_CLASSES)
+
+### Miscellaneous Config
+MODEL_PREFIX = "model_name"
+BATCH_SIZE = 16
+RANDOM_SEED = 629
+
+### GPU SETTINGS
+CUDA_DEVICE = 0  # GPU device ID
+GPU_MODE = torch.cuda.is_available()
